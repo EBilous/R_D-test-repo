b719cb6e18c6b713c3cea606318671378e57564d
Eugene Bilous <eugene.bilous@egym.com>
2023-05-26 11:02:03 +0300

    update

--- Main.java
+++
@@ -1,3 +0,0 @@
-public class Main {
-    // "Я починаю курс Java в robot_dreams і вже вмію використовувати Git"
-}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
+    }
+}
