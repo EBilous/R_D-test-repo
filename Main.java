22f7cdacc9dfc944dc8cdf6c28620ffdc33f9cb5
Eugene Bilous <eugene.bilous@egym.com>
2023-05-26 09:15:54 +0300

    HW1

---
+++ Main.java
@@ -0,0 +1,3 @@
+public class Main {
+    // "Я починаю курс Java в robot_dreams і вже вмію використовувати Git"
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # R_D-test-repo
 Test repo for Java courses
+
+This project is used as playground for Java course
