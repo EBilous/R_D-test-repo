e50d5704bd8279f77b3029431fd28c8e06048d21
Eugene Bilous <eugene.bilous@egym.com>
2023-05-31 09:16:35 +0300

    HW_2

---
+++ src/home_work/HW2_data_types.java
@@ -0,0 +1,14 @@
+package src.home_work;
+
+
+public class HW2_data_types {
+    public static void main(String[] args) {
+        int first = 2000;
+        int second = 333;
+
+        System.out.printf("Sum of variables %s and %s = " + (first + second) + "%n", first, second);
+        System.out.printf("Subtraction of variables %s and %s = " + (first - second) + "%n", first, second);
+        System.out.printf("Multiplication of variables %s and %s = " + (first * second) + "%n", first, second);
+        System.out.printf("Division of variables %s and %s = " + ((float) first / second) + "%n", first, second);
+    }
+}
