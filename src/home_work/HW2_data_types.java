c3f494265cbd453c00fbeae637d1489a53c09e47
Eugene Bilous <eugene.bilous@egym.com>
2023-06-02 12:17:54 +0300

    Scanner

--- src/home_work/HW2_data_types.java
+++ src/home_work/HW2_data_types.java
@@ -1,14 +1,25 @@
 package src.home_work;
 
 
+import java.util.Scanner;
+
 public class HW2_data_types {
     public static void main(String[] args) {
-        int first = 2000;
-        int second = 333;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the first integer: ");
+        int first = Integer.parseInt(input.nextLine());
 
+        System.out.println("Please enter the second integer: ");
+        int second = Integer.parseInt(input.nextLine());
+
+        // %s is a placeholder for a variable passed in parameters of the method
+        /* %n is a line break character which is used to print output in a new line, as printf doesn't add new line by default
+        unlike the println method.
+        * */
         System.out.printf("Sum of variables %s and %s = " + (first + second) + "%n", first, second);
         System.out.printf("Subtraction of variables %s and %s = " + (first - second) + "%n", first, second);
         System.out.printf("Multiplication of variables %s and %s = " + (first * second) + "%n", first, second);
         System.out.printf("Division of variables %s and %s = " + ((float) first / second) + "%n", first, second);
+        System.out.println(String.format("Is %s greater than %s? ", first, second) + (first>second));
     }
 }
