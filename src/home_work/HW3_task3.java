e90011a50d2a670fc69aeff6de4f9f1f825f4b1a
Eugene Bilous <eugene.bilous@egym.com>
2023-06-05 23:04:44 +0300

    Review fixes

--- src/home_work/HW3_task3.java
+++ src/home_work/HW3_task3.java
@@ -1,17 +1,20 @@
 package src.home_work;
 
-import org.jetbrains.annotations.NotNull;
-
-
 /*Якщо хтось справиться швидко то ось ще одне :
     Напишіть метод, який приймає два рядки - основний текст і слово для пошуку - і повертає кількість разів, коли слово з'являється в тексті.
     input :  {Try your skill, your},    {Try test in your test life, test }
     Output : 1, 2;
     в першому випадку слово your зустрічається 1 раз, в другому випадку слово test зустрічається 2 рази*/
+
 public class HW3_task3 {
-    public static int myMethod(@NotNull String body, @NotNull String keyword){
+    public static int findByKeyword(String body, String keyword) {
+        // check body and keyword for null and empty values
+        if (body == null || keyword == null || body.isEmpty() || keyword.isEmpty()) {
+            return 0;
+        }
+
         int itemsFound = 0;
-        String[] words = body.toLowerCase().replaceAll("\\p{Punct}", "").split(" ");
+        String[] words = body.replaceAll("\\p{Punct}", "").split(" ");
         for (String i: words) {
             if (i.equals(keyword))
                 itemsFound++;
@@ -19,7 +22,12 @@ public class HW3_task3 {
         return itemsFound;
     }
     public static void main(String[] args) {
-        System.out.println(myMethod("Try your skill", "your"));
-        System.out.println(myMethod("Try test in your test life", "test"));
+        System.out.println(findByKeyword("Try your skill", "your"));
+        System.out.println(findByKeyword("Try your skill", null)); // additional case with null keyword
+
+        System.out.println(findByKeyword("Try test in your test life", "test"));
+        System.out.println(findByKeyword("", "test")); //additional case with empty body
+
+
     }
 }
