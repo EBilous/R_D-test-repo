3453f3320d4d7e92297f0cd38f05679d9a623a91
Eugene <32193391+EBilous@users.noreply.github.com>
2023-08-08 20:43:30 +0300

    LEC19 Vocabulary UA (#21)

---
+++ src/home_work/LEC17/collections/LEC19/maps/Main.java
@@ -0,0 +1,25 @@
+package src.home_work.LEC17.collections.LEC19.maps;
+
+/*Мапа має містити інформацію про 10 будь-яких слів українською мовою і її переклад на: англійску, японську, німецьку.
+ Коли користувач дає мапі слово українською - то потрібен отримати у результаті переклад на ці 3 мови*/
+
+
+/*  "привіт"
+    "дякую"
+    "будь ласка"
+    "кохання"
+    "добре"
+    "робота"
+    "школа"
+    "сонце"
+    "вода"
+    "мир"   */
+
+import static src.home_work.LEC17.collections.LEC19.maps.UkrainianTranslations.translate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        translate();
+    }
+}
